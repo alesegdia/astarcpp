6092b2831e7a5ecc31df6487259a2386893e0596
Alejandro Seguí <alesegdia@gmail.com>
2016-08-21 22:41:32 +0200

    lots of refactoring

--- src/astar.h
+++ src/astar.h
@@ -14,39 +14,36 @@ template <typename NodeType>
 class AStarNode
 {
 public:
-	float FCost() const
+	float F() const
 	{
-		return GCost() + HCost();
+		return m_fCost;
 	}
-	float GCost() const
+	float G() const
 	{
 		return m_gCost;
 	}
-	float HCost() const
+	float H() const
 	{
 		return m_hCost;
 	}
-	void HCost( float hcost )
+	void H( float hcost )
 	{
 		m_hCost = hcost;
 	}
-	void GCost( float gcost )
+	void G( float gcost )
 	{
 		m_gCost = gcost;
 	}
-	NodeType* Parent() const
+
+	void computeF()
 	{
-		return m_parent;
-	}
-	void Parent( NodeType* node )
-	{
-		m_parent = node;
+		m_fCost = m_gCost + m_hCost;
 	}
 
 private:
 	float m_gCost = 0 ;
 	float m_hCost = 0 ;
-	NodeType* m_parent;
+	float m_fCost = 0 ;
 
 };
 
@@ -54,7 +51,7 @@ template <typename ProblemModel>
 class AStar
 {
 	typedef typename ProblemModel::Ptr ProblemModelPtr;
-	typedef typename ProblemModel::Node::Ptr NodePtr;
+	typedef typename ProblemModel::NodePtr NodePtr;
 
 	struct NodeCompare
 	{
@@ -78,38 +75,22 @@ public:
 
 	}
 
-	~AStar()
-	{
-		cleanLists();
-	}
-
-	void cleanLists()
-	{
-		for( auto item : m_openList )
-		{
-			delete item;
-		}
-
-		for( auto item : m_closeList )
-		{
-			delete item;
-		}
-	}
-
 	void restartSearch( NodePtr start_node, NodePtr target_node )
 	{
-		cleanLists();
+		m_closeList.clear();
+		m_openList.clear();
 		m_targetNode = target_node;
 		m_openList.push_back(start_node);
+		m_processedNodes = 0;
 	}
 
 	bool step()
 	{
+		// stats
+		m_processedNodes++;
+
 		bool ret = false;
-		std::sort(m_openList.begin(), m_openList.end(),
-				  [](NodePtr n1, NodePtr n2) { return n1->GCost() > n2->GCost(); });
-		NodePtr current_node = m_openList.back();
-		m_openList.erase(m_openList.begin() + m_openList.size() - 1);
+		NodePtr current_node = popBestNode();
 		if( m_problemModel->equal(m_targetNode, current_node) )
 		{
 			ret = true;
@@ -123,49 +104,59 @@ public:
 		return ret;
 	}
 
+	size_t processedNodes()
+	{
+		return m_processedNodes;
+	}
+
+private:
+
+	NodePtr popBestNode()
+	{
+		sortByCost( m_openList );
+		NodePtr best = m_openList.back();
+		m_openList.erase(m_openList.begin() + m_openList.size() - 1);
+		return best;
+	}
+
+	void sortByCost( std::vector<NodePtr>& v )
+	{
+		std::sort(v.begin(), v.end(), [](NodePtr n1, NodePtr n2) { return n1->F() > n2->F(); });
+	}
+
 	void processNode( NodePtr current_node )
 	{
 		std::vector<NodePtr> neighboors = m_problemModel->getNeighboors(current_node, m_targetNode);
 		for( NodePtr neigh : neighboors )
 		{
-			auto checker = [&](NodePtr item) {
-				return m_problemModel->equal( neigh, item );
-			};
-
-			auto found_in_open = std::find_if( m_openList.begin(), m_openList.end(), checker );
-
-			if( found_in_open != m_openList.end() )
+			if( hasBetterEqualNode( m_openList, neigh ) || hasBetterEqualNode( m_closeList, neigh ) )
 			{
-				if( ((*found_in_open)->FCost()) > neigh->FCost() )
-				{
-					delete neigh;
-					continue;
-				}
+				continue;
 			}
 
-			auto found_in_close = std::find_if( m_closeList.begin(), m_closeList.end(), checker );
+			removeDuplicates( m_closeList, neigh );
+			removeDuplicates( m_openList, neigh );
 
-			if( found_in_close != m_closeList.end() )
-			{
-				if( ((*found_in_close)->FCost()) > neigh->FCost() )
-				{
-					delete neigh;
-					continue;
-				}
-			}
-
-			m_closeList.erase( std::remove_if( m_closeList.begin(), m_closeList.end(), checker ), m_closeList.end() );
-			m_openList.erase(  std::remove_if( m_openList.begin(),  m_openList.end(),  checker ), m_openList.end() );
-
+			neigh->Parent(current_node);
 			m_openList.push_back(neigh);
 		}
 	}
 
-private:
+	bool hasBetterEqualNode( std::vector<NodePtr>& v, NodePtr node )
+	{
+		auto found = std::find_if( v.begin(), v.end(), [&](NodePtr item) { return m_problemModel->equal( node, item ); } );
+		return found != v.end() && ((*found)->G()) < node->G();
+	}
+
+	void removeDuplicates( std::vector<NodePtr>& v, NodePtr node )
+	{
+		v.erase( std::remove_if( v.begin(), v.end(), [&](NodePtr item) { return m_problemModel->equal( node, item ); } ), v.end() );
+	}
+
 	ProblemModelPtr m_problemModel;
-	//std::priority_queue<NodePtr, std::vector<NodePtr>, NodeCompare> m_openList;
 	std::vector<NodePtr> m_openList;
 	std::vector<NodePtr> m_closeList;
 	NodePtr m_targetNode;
+	size_t m_processedNodes = 0;
 
 };
